3a7034cf18ca2130ed1579029a3638c1b0cb1911
Timur Salikhov <timur.dev@mail.ru>
2023-07-21 01:14:13 +0300

    feat: final 2 task

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,97 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import {
+    __,
+    allPass,
+    andThen,
+    assoc,
+    compose,
+    concat,
+    gt,
+    ifElse,
+    length,
+    lt,
+    mathMod,
+    otherwise,
+    partial,
+    prop,
+    tap,
+    test,
+} from 'ramda';
+import Api from '../tools/api';
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+// number comparison
+const greaterThenTwo = gt(__, 2);
+const lowerThenTen = lt(__, 10);
+const square = (num) => num ** 2;
+const modForThree = mathMod(__, 3);
+const thenSquare = andThen(square);
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+// string
+const lengthGreaterThenTwo = compose(greaterThenTwo, length);
+const lengthLowerThenTen = compose(lowerThenTen, length);
+const testOnlyNumber = test(/^[0-9]+\.?[0-9]+$/);
+const stringToRoundNumber = compose(Math.round, Number);
+const thenGetLength = andThen(length);
+const modForThreeString = compose(String, modForThree);
+const thenModForThreeString = andThen(modForThreeString);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+// validation
+const validate = allPass([
+    lengthGreaterThenTwo,
+    lengthLowerThenTen,
+    testOnlyNumber,
+]);
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+// api
+const API_NUMBERS_URL = 'https://api.tech/numbers/base';
+const API_ANIMALS_URL = 'https://animals.tech/';
+const apiGetNumbers = api.get(API_NUMBERS_URL);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const getApiResult = compose(String, prop('result'));
+const thenGetApiResult = andThen(getApiResult);
+const setNumberToApiProp = assoc('number', __, { from: 10, to: 2 });
+const thenConcatToAnimalUrl = andThen(concat(API_ANIMALS_URL));
+const thenCallApiGetAnimals = andThen(api.get(__, {}));
+const apiGetNumber = compose(apiGetNumbers, setNumberToApiProp);
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+// main function
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+    const tapLog = tap(writeLog);
+    const thenTapLog = andThen(tapLog);
+    const thenTapSuccess = andThen(handleSuccess);
+    const otherwiseTapError = otherwise(handleError);
+    const tapValidateError = partial(handleError, ['ValidationError']);
+
+    const sequenceComposition = compose(
+        otherwiseTapError,
+        thenTapSuccess,
+        thenGetApiResult,
+        thenCallApiGetAnimals,
+        thenConcatToAnimalUrl,
+        thenTapLog,
+        thenModForThreeString,
+        thenTapLog,
+        thenSquare,
+        thenTapLog,
+        thenGetLength,
+        thenTapLog,
+        thenGetApiResult,
+        apiGetNumber,
+        tapLog,
+        stringToRoundNumber
+    );
+
+    const runWithCondition = ifElse(
+        validate,
+        sequenceComposition,
+        tapValidateError
+    );
+    const logAndRunSequence = compose(runWithCondition, tapLog);
+    logAndRunSequence(value);
+};
 
 export default processSequence;
